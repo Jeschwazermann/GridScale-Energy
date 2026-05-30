06f46ae752088b2a21816e320220b1eee1aac7b9
Jeschwazermann <slaetunechi@gmail.com>
2026-05-30 20:54:12 +0100

    fix: update API URL in .env and ensure fallback in axios configuration

--- client/src/components/services/api.js
+++ client/src/components/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: import.meta.env.VITE_API_URL || "https://gridscale-energy.onrender.com",
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "https://gridscale-energy.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
