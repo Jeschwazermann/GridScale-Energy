6cdee5bf3618498019dce4cc46cdbf610c3fc46b
Jeschwazermann <slaetunechi@gmail.com>
2026-06-18 00:58:02 +0100

    fix: correct import statement for calculatorRoutes in app.js

--- server/app.js
+++ server/app.js
@@ -1,26 +1,7 @@
-// import express from "express";
-// import cors from "cors";
-// import calculatorRoute from "./routes/calculatorRoute.js";
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api", calculatorRoute);
-
-// app.use((err, req, res, next) => {
-//   console.error(`[ERROR] ${req.method} ${req.path}:`, err.message);
-//   res.status(err.status || 500).json({
-//     error: err.message || "Internal server error",
-//   });
-// });
-
-// export default app;
 
 import express from "express";
 import cors from "cors";
-import calculatorRoutes from "./routes/calculatorRoutes.js";
+import calculatorRoutes from "./routes/calculatorRoute.js";
 import installerRoutes from "./routes/installerRoute.js";
 import leadRoutes from "./routes/leadRoute.js";
 
