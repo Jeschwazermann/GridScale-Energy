f1259108614309333ed18e2d541d8f33d0f0c3c9
Jeschwazermann <slaetunechi@gmail.com>
2026-06-20 09:33:42 +0100

    fix: comment out variables

--- client/src/lib/supabase.js
+++ client/src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnon = import.meta.env.VITE_SUPABASE_ANON_KEY;
+//const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+//const supabaseAnon = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnon) {
   throw new Error(
